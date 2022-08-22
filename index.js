12a8aab1b3a4876e1fad67f2358e109ad7e63a0a
nelsonmutindamwanzia <nmutinda02@gmail.com>
2022-08-22 11:29:27 +0300

    Complete Assignment

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+const input = document.querySelector('input');
+
 function addingEventListener() {
+   input.addEventListener('click', function(event) {
+       return alert('The Button was clicked!');
+   });
 }
+
+
+// const input = document.querySelector('input');
+
+// function addingEventListener() {
+//   input.addEventListener('click', function(event) {
+//     return "clicked"
+//   });
+// }
